5af304aabff741f22d07a9c6be4e1246ca2de746
hughsilva <hugosilva.b18@gmail.com>
2017-08-02 01:20:31 -0300

    Primeiro Envio

---
+++ Assets/Scripts/movimentarUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movimentarUI : MonoBehaviour {
+	public Image barco;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoverUI();
+	}
+    public void MoverUI()
+    {
+		barco.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+    }
+}
